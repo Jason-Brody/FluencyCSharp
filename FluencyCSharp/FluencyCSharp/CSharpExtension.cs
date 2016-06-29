17d5be226b7f9e49db452cf662d0d538219176d3
Zhou Yang <kav7860@gmail.com>
2016-06-29 22:38:00 +0800

    Update

---
+++ FluencyCSharp/FluencyCSharp/CSharpExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FluencyCSharp
+{
+    public static class CSharpExtension
+    {
+        public static T AppendTo<T>(this T item, ICollection<T> collections) {
+            collections.Add(item);
+            return item;
+            
+            
+        }
+
+        
+
+       
+
+        public static void SaveToXml<T>(this T obj, string file) where T:new()  {
+            XmlSerializer xs = new XmlSerializer(obj.GetType());
+            using (var fs = new FileStream(file, FileMode.Create)) {
+                xs.Serialize(fs, obj);
+            }
+        }
+    }
+}
---
+++ FluencyCSharp/FluencyCSharp/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FluencyCSharp
+{
+    public static class Utils
+    {
+        public static T GetObjFromXml<T>(string file) where T:new() {
+            if (!File.Exists(file))
+                return new T();
+
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            using (var fs = new FileStream(file, FileMode.Open)) {
+                return (T)xs.Deserialize(fs);
+            }
+
+
+        }
+    }
+}
